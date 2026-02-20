aca32ab2a26122f1f5da0b653b34a7b50488eea2
jd <hashimweyesujeandedie@gmail.com>
2026-02-20 11:54:06 +0200

    Configured vehicles_dropdown.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        infiniti: ['"Infiniti Brand Light"', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 };
