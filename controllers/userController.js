029439dd3a11c64bb2f837b58dc32c3bfd60448d
jd <hashimweyesujeandedie@gmail.com>
2026-03-02 11:16:14 +0200

    userController deployed from controllers

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require("../models/user")
+
+const registerUser = async(req,res) => {
+    try{
+        const {name, email, password} = req.body;
+
+        // SIMPLE VALIDATION
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            });
+        }
+
+        // CHECK IF USER EXISTS
+        const userExists = await User.findOne({email});
+
+        if (userExists){
+            return res.status(400).json({
+                success: false,
+                message:"User already Exists"
+            });
+        }
+
+        // CREATE USER
+
+        const user =  await User.create({
+            name,
+            email,
+            password
+        });
+
+        // SEND RESPONSE
+
+        res.status(201).json({
+            success:true,
+            message: 'User registered successfully',
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch(error){
+        console.error("Registration error:", error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    };
+
+    module.exports ={
+        registerUser
+    }
+}
